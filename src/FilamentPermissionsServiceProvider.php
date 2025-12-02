34f7a80581b5fbb4c096ed47eb0d2fffc2fd29ed
Ashok Kumar <aktilokchandani@hotmail.com>
2025-12-03 04:31:27 +0500

    Add FilamentPermissionsServiceProvider and resources registration

--- src/FilamentPermissionsServiceProvider.php
+++ src/FilamentPermissionsServiceProvider.php
@@ -4,18 +4,29 @@ namespace WhiteDev\FilamentPermissions;
 
 use Illuminate\Support\ServiceProvider;
 use WhiteDev\FilamentPermissions\Commands\GeneratePermissions;
+use Filament\Facades\Filament;
+use WhiteDev\FilamentPermissions\Resources\RoleResource;
+use WhiteDev\FilamentPermissions\Resources\PermissionResource;
 
 class FilamentPermissionsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        // Publish config
         $this->publishes([
             __DIR__.'/../config/filament-permissions.php' => config_path('filament-permissions.php'),
         ], 'config');
 
+        // Register command
         if ($this->app->runningInConsole()) {
             $this->commands([GeneratePermissions::class]);
         }
+
+        // **Register Filament Resources**
+        Filament::registerResources([
+            RoleResource::class,
+            PermissionResource::class,
+        ]);
     }
 
     public function register(): void
