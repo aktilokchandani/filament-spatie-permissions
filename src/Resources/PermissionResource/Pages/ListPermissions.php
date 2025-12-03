3ac3cbce58b6888de7859677980e5ed9cb6ed485
Ashok Kumar <aktilokchandani@hotmail.com>
2025-12-04 02:40:32 +0500

    Filament permissions resource plugin added

--- src/Resources/PermissionResource/Pages/ListPermissions.php
+++ src/Resources/PermissionResource/Pages/ListPermissions.php
@@ -1,55 +1,21 @@
 <?php
 
-namespace WhiteDev\FilamentPermissions\Resources\PermissionResource\Pages;
+namespace App\Filament\Resources\PermissionResource\Pages;
 
-use WhiteDev\FilamentPermissions\Resources\PermissionResource\Pages;
-use WhiteDev\FilamentPermissions\Models\Permission;
-use Filament\Resources\Resource;
-use Filament\Forms\Form;
-use Filament\Tables\Table;
-use Filament\Tables\Columns\TextColumn;
+use App\Filament\Resources\PermissionResource;
+use Filament\Resources\Pages\ListRecords;
 
-class PermissionResource extends Resource
+class ListPermissions extends ListRecords
 {
-    protected static ?string $model = Permission::class;
-    protected static ?string $navigationIcon = 'heroicon-s-cog';
-    protected static ?string $navigationGroup = 'App Settings';
+    protected static string $resource = PermissionResource::class;
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([]);
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                TextColumn::make('id')
-                    ->sortable()
-                    ->size('sm'),
-                TextColumn::make('name')
-                    ->label('Name')
-                    ->sortable()
-                    ->size('sm'),
-                TextColumn::make('guard_name')
-                    ->label('Guard Name')
-                    ->sortable()
-                    ->size('sm'),
-            ])
-            ->filters([])
-            ->actions([]);
-    }
-
-    public static function getRelations(): array
+    protected function getActions(): array
     {
         return [];
     }
 
-    public static function getPages(): array
+    protected function getTableRecordsPerPageSelectOptions(): array
     {
-        return [
-            'index' => Pages\ListPermissions::route('/'),
-        ];
+        return [25, 50, 100];
     }
 }
